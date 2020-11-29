9db545b75d9bdf6a929022b872751b2d5cb785eb
JNagasava <jnagasava@usp.br>
2020-11-29 16:57:59 -0300

    code refactored

--- dynamic_graph.py
+++ dynamic_graph.py
@@ -1,17 +1,32 @@
 import matplotlib.pyplot as plt
-from matplotlib.artist import Artist
 from matplotlib.widgets import Slider, Button, CheckButtons
 
 from irt import IRT
 
 class Dynamic_Graph:
+    '''
+    Classe responsavel pelo grafico dinamica da TRI
+    Neste grafico sera possivel realizar comparacoes 2 modelos
+    e comparacoes entre a Curva caracteristica do item 
+    pela Curva de Informacao do Item
+    '''
     
     def __init__(self, model=IRT()):
-        
+        '''
+        Funcao construtora da classe
+        Nela sera feito a inicializacao do plot bem como
+        as configuracoes iniciais do modelo de TRI
+
+        Parametros
+        ----------
+        model : irt.IRT
+            modelo de 3 parametros
+        '''
+
         # Model e XY
         self.model = model
-        self.x_icc, self.y_icc = model.icc_xy()
-        self.x_iic, self.y_iic = model.iic_xy()
+        self.x_icc, self.y_icc = model.icc()
+        self.x_iic, self.y_iic = model.iic()
 
         # Subplot
         self.fig, self.ax = plt.subplots()
@@ -21,6 +36,7 @@ class Dynamic_Graph:
         self.info_plot()
         plt.grid()
 
+        # Curvas do Model
         self.p_icc, = plt.plot(self.x_icc, self.y_icc, linestyle='solid',linewidth=2, color='blue')
         self.p_iic, = plt.plot(self.x_iic, self.y_iic, linestyle='dashed', linewidth=1, color='blue')
         self.p_iic.set_visible(False)
@@ -57,17 +73,23 @@ class Dynamic_Graph:
         self.btn.on_clicked(self.reset)
 
     def create_second_model(self):
+        '''
+        Cria um segundo modelo para usar, como comparacao,
+        no plot e realiza as configuracoes necessarias
+        '''
 
+        # 2nd Model e XY
         self.second_model = IRT(a=self.model.a, b=self.model.b, c=self.model.c, theta_min=self.model.theta_min, theta_max=self.model.theta_max)
-        self.second_x_icc, self.second_y_icc = self.second_model.icc_xy()
-        self.second_x_iic, self.second_y_iic = self.second_model.iic_xy()
+        self.second_x_icc, self.second_y_icc = self.second_model.icc()
+        self.second_x_iic, self.second_y_iic = self.second_model.iic()
 
+        # Curvas do 2nd Model
         self.second_p_icc, = plt.plot(self.second_x_icc, self.second_y_icc, linestyle='solid',linewidth=2, color='red')
         self.second_p_iic, = plt.plot(self.second_x_iic, self.second_y_iic, linestyle='dashed', linewidth=1, color='red')
-
         self.second_p_icc.set_visible(False)
         self.second_p_iic.set_visible(False)
 
+        # Sliders do 2nd Model 
         self.second_sliders = {'a': self.add_slider(axes=[0.6, 0.23, 0.25, 0.05], 
                                                     label=r'$\mathbb{a}$', 
                                                     valmin=0.0, 
@@ -93,11 +115,51 @@ class Dynamic_Graph:
         self.second_sliders['c'].ax.set_visible(False)
 
     def info_plot(self, title=r'$ICC$', xlabel=r'$\theta$', ylabel=r'$P(\theta)$'):
+        '''
+        Atribui o titulo do grafico e o significado
+        dos eixos x e y
+
+        Parametros
+        ----------
+            title : str
+                titulo do grafico
+            xlabel : str
+                nome do eixo x
+            ylabel : str
+                nome do eixo y
+        '''
         self.ax.set_title(title)
         self.ax.set_xlabel(xlabel)
         self.ax.set_ylabel(ylabel)
 
     def add_slider(self, axes, label, valmin, valmax, valinit, valfmt="%1.2f", closedmax=True, color=None):
+        '''
+        Cria um novo slider a partir de determinadas configuracoes
+
+        Parametros
+        ----------
+            axes : list(float, float, float, float)
+                lista com a posicao do slider -> list(left, up, right, bottom)
+            label : str
+                nome do parametro do slider
+            valmin : float
+                valor minimo do parametro
+            valmax : float
+                valor maximo do parametro
+            valinit : float
+                valor inicial do parametro
+            valfmt : str
+                formatacao do valor corrente do parametro
+            closedmax : bool
+                indica se eh permitido usar o valor maximo do parametro
+            color : str
+                cor da barra do slider
+        
+        Retorno
+        -------
+            matplotlib.widgets.Slider
+                retorna o slider criado
+        '''
         axSlider = plt.axes(axes)
         slider = Slider(ax=axSlider, 
                         label=label, 
@@ -111,6 +173,16 @@ class Dynamic_Graph:
         return slider
     
     def update_sliders(self, second_model=False):
+        '''
+        Atualiza os sliders
+
+        Parametros
+        ----------
+            second_model : bool
+                indica qual slider esta atualizando. Se second_model for False entao 
+                atualiza o slider do model padrao, caso contrario atualiza o slider
+                do second_model
+         '''
 
         if not second_model:
             self.sliders['a'].on_changed(self.update_model)
@@ -123,61 +195,105 @@ class Dynamic_Graph:
             self.second_sliders['c'].on_changed(self.update_second_model)
 
     def update_model(self, val=None):
+        '''
+        Atualiza os valores do model e dependedo da situacao
+        aumenta o limite de visualizacao do eixo y no grafico
 
+        Parametros
+        ----------
+            val : float
+                valor corrente do parametro (nao eh utilizado nesta funcao)
+        '''
+
+        # Atualiza os valores do model
         self.model.a = self.sliders['a'].val
         self.model.b = self.sliders['b'].val
         self.model.c = self.sliders['c'].val
-        self.y_icc = self.model.icc_xy()[1]
+        self.y_icc = self.model.icc()[1]
         self.p_icc.set_ydata(self.y_icc)
-        self.y_iic = self.model.iic_xy()[1]
+        self.y_iic = self.model.iic()[1]
         self.p_iic.set_ydata(self.y_iic)
 
-        if self.p_iic.get_visible() and max([max(self.y_iic), max(self.second_y_iic)]) > 1.0:
-            self.ax.set_ylim([-0.1, max([max(self.y_iic), max(self.second_y_iic)]) + 0.1])
+        # Verifica o limite do eixo y
+        if self.p_iic.get_visible():
+            if self.second_p_iic.get_visible():
+                self.ax.set_ylim([-0.1, max([1.0, max(self.y_iic), max(self.second_y_iic)]) + 0.1])
+            else:
+                self.ax.set_ylim([-0.1, max([1.0, max(self.y_iic)]) + 0.1])
         else:
             self.ax.set_ylim([-0.1, 1.1])
 
         plt.draw()
     
     def update_second_model(self, val=None):
+        '''
+        Atualiza os valores do second_model e dependedo da situacao
+        aumenta o limite de visualizacao do eixo y no grafico
 
+        Parametros
+        ----------
+            val : float
+                valor corrente do parametro (nao eh utilizado nesta funcao)
+        '''
+        
+        # Atualiza os valores do second_model
         self.second_model.a = self.second_sliders['a'].val
         self.second_model.b = self.second_sliders['b'].val
         self.second_model.c = self.second_sliders['c'].val
-        self.second_y_icc = self.second_model.icc_xy()[1]
+        self.second_y_icc = self.second_model.icc()[1]
         self.second_p_icc.set_ydata(self.second_y_icc)
-        self.second_y_iic = self.second_model.iic_xy()[1]
+        self.second_y_iic = self.second_model.iic()[1]
         self.second_p_iic.set_ydata(self.second_y_iic)
 
-        if self.p_iic.get_visible() and max([max(self.y_iic), max(self.second_y_iic)]) > 1.0:
-            self.ax.set_ylim([-0.1, max([max(self.y_iic), max(self.second_y_iic)]) + 0.1])
+        # Verifica o limite do eixo y
+        if self.p_iic.get_visible():
+            if self.second_p_iic.get_visible():
+                self.ax.set_ylim([-0.1, max([1.0, max(self.y_iic), max(self.second_y_iic)]) + 0.1])
+            else:
+                self.ax.set_ylim([-0.1, max([1.0, max(self.y_iic)]) + 0.1])
         else:
             self.ax.set_ylim([-0.1, 1.1])
 
         plt.draw()
     
-    def check(self, label):
+    def check(self, label=None):
+        '''
+        Verifica o status dos checkbuttons para gerenciar o grafico.
+        Dependendo da situacao, o grafico podera ter a Curva de Informacao
+        do Item, a Curva Caracteristica do Item e um Segundo Modelo para servir
+        de comparacao
 
+        Parametros
+        ----------
+            label : str
+                nome do checkbutton que mudou de estado (nao eh utilizado nesta funcao)
+        '''
+
+        # ICC e IIC
         if self.checkbtns.get_status()[0] and self.checkbtns.get_status()[1]:
             self.p_icc.set_visible(True)
             self.p_iic.set_visible(True)
             self.info_plot(title=r'$ICC$~$IIC$', xlabel=r'$\theta$', ylabel=r'$P(\theta)$~$I(\theta)$')
         
+        # ICC
         elif self.checkbtns.get_status()[0] and not self.checkbtns.get_status()[1]:
             self.p_icc.set_visible(True)
             self.p_iic.set_visible(False)
             self.info_plot(title=r'$ICC$', xlabel=r'$\theta$', ylabel=r'$P(\theta)$')
 
+        # IIC
         elif not self.checkbtns.get_status()[0] and self.checkbtns.get_status()[1]:
             self.p_icc.set_visible(False)
             self.p_iic.set_visible(True)
             self.info_plot(title=r'$IIC$', xlabel=r'$\theta$', ylabel=r'$I(\theta)$')
         
+        # Nothing
         elif not self.checkbtns.get_status()[0] and not self.checkbtns.get_status()[1]:
             self.p_icc.set_visible(False)
             self.p_iic.set_visible(False)
             self.info_plot(title='', xlabel='', ylabel='')
         
+        # 2nd Model - on
         if self.checkbtns.get_status()[2]:
 
                 self.sliders['a'].ax.set_position([0.25, 0.23, 0.25, 0.05])
@@ -191,6 +307,7 @@ class Dynamic_Graph:
                 self.second_sliders['b'].ax.set_visible(True)
                 self.second_sliders['c'].ax.set_visible(True)
 
+        # 2nd Model - off
         else:
             self.second_p_icc.set_visible(False)
             self.second_p_iic.set_visible(False)
@@ -207,7 +324,11 @@ class Dynamic_Graph:
         self.update_second_model(None)
 
     def reset(self, val=None):
-
+        '''
+        Funcao de reset do plot, que quando eh acionado, volta-se
+        para os valores iniciais do modelo
+        '''
+        
         self.sliders['a'].reset()
         self.sliders['b'].reset()
         self.sliders['c'].reset()
@@ -221,4 +342,13 @@ class Dynamic_Graph:
 
 
     def plot_show(self):
+        '''
+        Inicia a exibicao do plot
+        '''
         plt.show()
+    
+    def plot_close(self):
+        '''
+        Fecha a exibicao do plot
+        '''
+        plt.close()
--- irt.py
+++ irt.py
@@ -2,28 +2,107 @@ from numpy import linspace
 from math import exp
 
 class IRT:
+    '''
+    Classe com as informacoes do modelo de 3 parametros
+    da TRI (Teoria de Resposta ao Item)
+    Esta classe tambem tem a funcao de realizar os calculos da probabilidade
+    de acerto de um item e da informacao do item 
+    '''
 
     def __init__(self, a=2.0, b=0.0, c=0.0, theta_min=-3.0, theta_max=3.0):
+        '''
+        Funcao construtora da classe a qual atribui os valores dos parametros
+        do modelo
+
+        Parametros
+        ----------
+            a : float
+                discriminate do item
+            b : float
+                dificuldade do item
+            c : float
+                chance de acerto ao acaso
+            theta_min : float
+                habilidade minima do individuo. Eh usado para a Curva Caracteristica
+                do Item e para a Curva de Informacao do Item
+            theta_max : float
+                habilidade maxima do individuo. Eh usado para a Curva Caracteristica
+                do Item e para a Curva de Informacao do Item
+        '''
         self.a = a
         self.b = b
         self.c = c
         self.theta_min = theta_min
         self.theta_max = theta_max
 
-    def icc(self, theta=float) -> float:
+    def probability(self, theta=float) -> float:
+        '''
+        Calcula a probabilidade de acertar o item partir da habilidade
+        do individuo
+
+        Parametros
+        ----------
+            theta : float
+                habilidade do individuo
+            
+        Retorno
+        -------
+            float
+                retorna a probabilidade de acertar o item
+        '''
         return self.c + ( 1.0 - self.c ) / ( 1.0 + exp( -1.0 * self.a * ( theta - self.b ) ) )
     
-    def icc_xy(self, dots=1001):
+    def icc(self, dots=1001):
+        '''
+        Gera os valores x e y da Curva Caracteristica do Item
+
+        Parametros
+        ----------
+            dots : int
+                quantidade de pontos na curva
+        
+        Retorno
+        -------
+            list(float...), list(float...)
+                retorna os valores de x e y da Curva
+        '''
         x = linspace(self.theta_min, self.theta_max, dots)
-        y = [self.icc(k) for k in x]
+        y = [self.probability(k) for k in x]
         return x, y
     
-    def iic(self, theta=float) -> float:
-        p = self.icc(theta)
+    def infomation(self, theta=float) -> float:
+        '''
+        Calcula a informacao do item a partir da habilidade theta
+        do individuo
+
+        Parametros
+        ----------
+            theta : float
+                habilidade do individuo
+        
+        Retorno
+        -------
+            float
+                retorna a informacao do item
+        '''
+        p = self.probability(theta)
         q = 1 - p
         return self.a**2 * ( ( p - self.c )**2 / ( 1 - self.c )**2 ) * ( q / p )
     
-    def iic_xy(self, dots=1001):
+    def iic(self, dots=1001):
+        '''
+        Gera os valores x e y da Curva de Informacao do Item
+
+        Parametros
+        ----------
+            dots : int
+                quantidade de pontos na curva
+        
+        Retorno
+        -------
+            list(float...), list(float...)
+                retorna os valores de x e y da Curva
+        '''
         x = linspace(self.theta_min, self.theta_max, dots)
-        y = [self.iic(k) for k in x]
+        y = [self.infomation(k) for k in x]
         return x, y
